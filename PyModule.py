52f621dc4806a96358b12384e685d9eb8a23d0f1
Andri Trottmann <55755273+Resistor10k1@users.noreply.github.com>
2022-11-13 21:11:50 +0100

    Add Python class

---
+++ PyModule.py
@@ -0,0 +1,27 @@
+# PyModule.py
+# Author: Andri Trottmann
+# Date: 13.11.22
+
+
+import ctypes as ct
+
+# load the library
+lib = ct.cdll.LoadLibrary('./libModule.so')
+
+class PyModule(object):
+    # constructor
+    def __init__(self):
+        self.obj = lib.Module_new()
+    
+    # destructor
+    def __del__(self):
+        lib.Module_del(self.obj)
+    
+    def printMsg(self):
+        lib.Module_print(self.obj)
+
+    def setVariable(self, number):
+        lib.Module_setVar(self.obj, number)
+
+    def getVariable(self):
+        return lib.Module_getVar(self.obj)
